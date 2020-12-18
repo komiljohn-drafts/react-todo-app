080e15847ea6b7074c3a04f690d3d7705a7c5045
Komiljon Yormurodov <komiljonyormurodov@gmail.com>
2020-12-18 22:03:45 +0500

    Updates

---
+++ precache-manifest.775678807195c3762df9ec8f30877381.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b2e0592ddd148b5acc69079937d74a",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "f0ac1775f0a77e60a436",
+    "url": "/react-todo-app/static/css/main.e10dce6e.chunk.css"
+  },
+  {
+    "revision": "a3b76cd9533a89f2fb53",
+    "url": "/react-todo-app/static/js/2.f1d96525.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.f1d96525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ac1775f0a77e60a436",
+    "url": "/react-todo-app/static/js/main.d1880cd7.chunk.js"
+  },
+  {
+    "revision": "f7bcd010a1bb2bd20ed6",
+    "url": "/react-todo-app/static/js/runtime-main.5452b913.js"
+  },
+  {
+    "revision": "a3e4c2f7c32ce9a7ad6fa8d381b9d9cf",
+    "url": "/react-todo-app/static/media/trash.a3e4c2f7.svg"
+  }
+]);
