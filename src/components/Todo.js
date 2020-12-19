90244e14cca9776d50fd5038a4a4ce49780b1339
Komiljon Yormurodov <komiljonyormurodov@gmail.com>
2020-12-19 23:32:24 +0500

    chore: Added adaptive styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,10 +5,10 @@ function Todo({ todo, handleChecked, handleRemove}) {
     return (
         <div>
             <li className="todo__item">
-                <input type="checkbox" onChange={() => handleChecked(todo.id)}/>
+                <input type="checkbox" checked={todo.completed} onChange={() => handleChecked(todo.id)}/>
                 <span>{todo.task}</span>
                 <button onClick={() => handleRemove(todo.id)}>
-                    <img src={TrashIcon} alt="Removing Todo" />
+                    <img src={TrashIcon} alt="Remove Todo" />
                 </button>
             </li>
         </div>
